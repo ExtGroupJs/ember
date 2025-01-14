0b281010b68bf41bd674d21bbccd8da8fa78f4b7
Drak <ealmeida.cu@gmail.com>
2025-01-14 16:38:16 -0500

    fixed errors

--- static/assets/dist/js/area.js
+++ static/assets/dist/js/area.js
@@ -8,13 +8,6 @@ const csrfToken = document.cookie
   ?.split("=")[1];
 // url del endpoint principal
 const url = "/user-gestion/employee-area/";
-// Variable con el token
-const csrfToken = document.cookie
-  .split(";")
-  .find((c) => c.trim().startsWith("csrftoken="))
-  ?.split("=")[1];
-// url del endpoint principal
-const url = "/user-gestion/employee-area/";
 
 $(document).ready(function () {
   $("table")
@@ -53,71 +46,7 @@ $(document).ready(function () {
         if (data.order[0].dir == "desc") {
           dir = "-";
         }
-  $("table")
-    .addClass("table table-hover")
-    .DataTable({
-      dom: '<"top"l>Bfrtip',
-      buttons: [
-        {
-          text: " Agregar",
-          className: " btn btn-primary btn-info",
-          action: function (e, dt, node, config) {
-            $("#modal-crear-elemento").modal("show");
-          },
-        },
-        {
-          extend: "excel",
-          text: "Excel",
-        },
-        {
-          extend: "pdf",
-          text: "PDF",
-        },
-        {
-          extend: "print",
-          text: "Imprimir",
-        },
-      ],
-      //Adding server-side processing
-      serverSide: true,
-      search: {
-        return: true,
-      },
-      processing: true,
-      ajax: function (data, callback, settings) {
-        dir = "";
-        if (data.order[0].dir == "desc") {
-          dir = "-";
-        }
 
-        axios
-          .get(url, {
-            params: {
-              page_size: data.length,
-              page: data.start / data.length + 1,
-              search: data.search.value,
-              ordering: dir + data.columns[data.order[0].column].data,
-            },
-          })
-          .then((res) => {
-            callback({
-              recordsTotal: res.data.count,
-              recordsFiltered: res.data.count,
-              data: res.data.results,
-            });
-          })
-          .catch((error) => {
-            alert(error);
-          });
-      },
-      columns: [
-        { data: "name", title: "Nombre" },
-        { data: "description", title: "Descripción" },
-        {
-          data: "",
-          title: "Acciones",
-          render: (data, type, row) => {
-            return `<div class="btn-group">
         axios
           .get(url, {
             params: {
@@ -171,34 +100,8 @@ $(document).ready(function () {
         },
       ],
     });
-          },
-        },
-      ],
-      //  esto es para truncar el texto de las celdas
-      columnDefs: [
-        {
-          targets: 1,
-          render: function (data, type, row) {
-            if (data == null || data == "") {
-              return (data = "Sin Datos");
-            } else {
-              return type === "display" && data.length > 80
-                ? data.substr(0, 80) + "…"
-                : data;
-            }
-          },
-        },
-      ],
-    });
 });
 
-$("#modal-eliminar-elemento").on("show.bs.modal", function (event) {
-  var button = $(event.relatedTarget); // Button that triggered the modal
-  var dataName = button.data("name"); // Extract info from data-* attributes
-  selected_id = button.data("id"); // Extract info from data-* attributes
-  var modal = $(this);
-  modal.find(".modal-body").text("¿Desea eliminar el área: " + dataName + "?");
-});
 $("#modal-eliminar-elemento").on("show.bs.modal", function (event) {
   var button = $(event.relatedTarget); // Button that triggered the modal
   var dataName = button.data("name"); // Extract info from data-* attributes
@@ -226,35 +129,8 @@ function function_delete(selected_id) {
         title: "El área no se pudo eliminar",
       });
     });
-  const table = $("#tabla-de-Datos").DataTable();
-  axios.defaults.headers.common["X-CSRFToken"] = csrfToken;
-  axios
-    .delete(`${url}${selected_id}/`)
-    .then((response) => {
-      Toast.fire({
-        icon: "success",
-        title: "El área seleccionada se eliminó correctamente",
-      });
-      table.row(`#${selected_id}`).remove().draw(); // use id selector to remove the row
-    })
-    .catch((error) => {
-      Toast.fire({
-        icon: "error",
-        title: "El área no se pudo eliminar",
-      });
-    });
 }
 
-$("#modal-crear-elemento").on("hide.bs.modal", (event) => {
-  // The form element is selected from the event trigger and its value is reset.
-  const form = event.currentTarget.querySelector("form");
-  form.reset();
-  // The 'edit_elemento' flag is set to false.
-  edit_elemento = false;
-  // An array 'elements' is created containing all the HTML elements found inside the form element.
-  const elements = [...form.elements];
-  // A forEach loop is used to iterate through each element in the array.
-  elements.forEach((elem) => elem.classList.remove("is-invalid"));
 $("#modal-crear-elemento").on("hide.bs.modal", (event) => {
   // The form element is selected from the event trigger and its value is reset.
   const form = event.currentTarget.querySelector("form");
@@ -292,33 +168,8 @@ $("#modal-crear-elemento").on("show.bs.modal", function (event) {
     modal.find(".modal-title").text("Crear Área");
   }
 });
-$("#modal-crear-elemento").on("show.bs.modal", function (event) {
-  var button = $(event.relatedTarget); // Button that triggered the modal
-  var modal = $(this);
-  if (button.data("type") == "edit") {
-    var dataName = button.data("name"); // Extract info from data-* attributes
-    var dataId = button.data("id"); // Extract info from data-* attributes
-    selected_id = button.data("id"); // Extract info from data-* attributes
-    edit_elemento = true;
-    modal.find(".modal-title").text("Editar " + dataName);
-    // Realizar la petición con Axios
-    axios
-      .get(`${url}${selected_id}/`)
-      .then(function (response) {
-        // Recibir la respuesta
-        const elemento = response.data;
-        // Llenar el formulario con los datos del usuario
-        form.elements.name.value = elemento.name;
-        form.elements.description.value = elemento.description;
-      })
-      .catch(function (error) {});
-  } else {
-    modal.find(".modal-title").text("Crear Área");
-  }
-});
 
 $(function () {
-  bsCustomFileInput.init();
   bsCustomFileInput.init();
 });
 
@@ -340,43 +191,7 @@ $(function () {
       },
     },
     submitHandler: function (form) {},
-  $.validator.setDefaults({
-    language: "es",
-    submitHandler: function () {
-      // alert("Form successful submitted!");
-    },
-  });
-  $("#form-create-elemento").validate({
-    rules: {
-      name: {
-        required: true,
-      },
-      description: {
-        required: true,
-      },
-    },
-    submitHandler: function (form) {},
 
-    messages: {
-      name: {
-        required: "El nombre del área es requerido",
-      },
-      description: {
-        required: "Debe proporcionar obligatoriamente una descripción",
-      },
-    },
-    errorElement: "span",
-    errorPlacement: function (error, element) {
-      error.addClass("invalid-feedback");
-      element.closest(".form-group").append(error);
-    },
-    highlight: function (element, errorClass, validClass) {
-      $(element).addClass("is-invalid");
-    },
-    unhighlight: function (element, errorClass, validClass) {
-      $(element).removeClass("is-invalid");
-    },
-  });
     messages: {
       name: {
         required: "El nombre del área es requerido",
@@ -406,16 +221,10 @@ form.addEventListener("submit", function (event) {
   var table = $("#tabla-de-Datos").DataTable();
   axios.defaults.headers.common["X-CSRFToken"] = csrfToken;
   if (form.checkValidity()) {
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  var table = $("#tabla-de-Datos").DataTable();
-  axios.defaults.headers.common["X-CSRFToken"] = csrfToken;
-  if (form.checkValidity()) {
     let data = new FormData();
     data.append("name", document.getElementById("name").value);
     data.append("description", document.getElementById("description").value);
 
-    const url = "/user-gestion/employee-area/";
     const url = "/user-gestion/employee-area/";
 
     if (edit_elemento) {
@@ -427,7 +236,7 @@ form.addEventListener("submit", function (event) {
               icon: "success",
               title: "Área editada con éxito",
               showConfirmButton: false,
-              timer: 2000,
+              timer: 50 * textError.length,
             });
             table.ajax.reload();
             $("#modal-crear-elemento").modal("hide");
@@ -449,14 +258,6 @@ form.addEventListener("submit", function (event) {
             timer: 50 * textError.length,
           });
         });
-          Swal.fire({
-            icon: "error",
-            title: "Error al crear usuario",
-            text: textError,
-            showConfirmButton: false,
-            timer: 50 * textError.length,
-          });
-        });
     } else {
       axios
         .post(url, data)
@@ -466,7 +267,7 @@ form.addEventListener("submit", function (event) {
               icon: "success",
               title: "Nueva área creada con éxito",
               showConfirmButton: false,
-              timer: 2000,
+              timer: 50 * textError.length,
             });
             table.ajax.reload();
             $("#modal-crear-elemento").modal("hide");
@@ -479,16 +280,6 @@ form.addEventListener("submit", function (event) {
             textError = textError + ", " + key + ": " + dict[key];
           }
 
-          Swal.fire({
-            icon: "error",
-            title: "Error al crear área",
-            text: textError,
-            showConfirmButton: false,
-            timer: 50 * textError.length,
-          });
-        });
-    }
-  }
           Swal.fire({
             icon: "error",
             title: "Error al crear área",
